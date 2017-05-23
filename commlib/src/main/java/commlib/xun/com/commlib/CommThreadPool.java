22ddb0e1df5eed717ddb32c660467d30c5a42d83
xwang <xunwang@wenba100.com>
2017-05-23 12:00:03 +0800

    first commit

---
+++ commlib/src/main/java/commlib/xun/com/commlib/CommThreadPool.java
@@ -0,0 +1,73 @@
+package commlib.xun.com.commlib;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * 公共线程池类
+ * Created by xunwang on 17/5/23.
+ */
+
+public class CommThreadPool {
+    private static volatile Executor sPool;
+    private static volatile Handler sHandler;
+    private static Handler sUiHandler = new Handler(Looper.getMainLooper());
+
+    public static final Executor getExecutor() {
+        if (sPool != null) {
+            return sPool;
+        }
+
+        synchronized (CommThreadPool.class) {
+            if (sPool == null) {
+                sPool = Executors.newCachedThreadPool();
+            }
+        }
+
+        return sPool;
+    }
+
+    public static final Handler getSerialHandler() {
+        if (sHandler != null) {
+            return sHandler;
+        }
+
+        synchronized (CommThreadPool.class) {
+            if (sHandler == null) {
+                HandlerThread thread = new HandlerThread("serial-looper");
+                thread.start();
+                sHandler = new Handler(thread.getLooper());
+            }
+        }
+
+        return sHandler;
+    }
+
+    /**
+     * 一般使用这个，在子线程中执行
+     * @param runnable
+     */
+    public static void poolExecute(Runnable runnable) {
+        getExecutor().execute(runnable);
+    }
+
+    /**
+     * 这个在主线程中执行
+     * @param runnable
+     */
+    public static void serialExecute(Runnable runnable) {
+        getSerialHandler().post(runnable);
+    }
+
+    public static void runOnUiThread(Runnable runnable) {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            runnable.run();
+        }
+
+        sUiHandler.post(runnable);
+    }
+}
